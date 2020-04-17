51be20995325f2485a141a75cff29a96fab07868
stevarino <oconnor39@gmail.com>
2020-04-17 16:23:58 -0700

    Update launcher again

--- bookshelf-lights.sh
+++ bookshelf-lights.sh
@@ -1,6 +1,19 @@
 # launcher script
 
+echo "Updating this directory"
 git pull
+
+echo ""
+echo "============================================="
+echo "Updating python requirements"
 pip3 install -r requirements.txt
+
+echo ""
+echo "============================================="
+echo "Running lights"
 python3 main.py
-# reboot
+
+echo ""
+echo "============================================="
+echo "Rebooting"
+# /sbin/reboot
